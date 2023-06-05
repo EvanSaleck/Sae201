f56a564964686fbc1333874d2a2879740bf8abc7
Evan Saleck <evan.saleck@gmail.com>
2023-06-05 11:41:38 +0200

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module fr.beastsheep.jeusociete {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens fr.beastsheep.jeusociete to javafx.fxml;
-    exports fr.beastsheep.jeusociete;
-}
